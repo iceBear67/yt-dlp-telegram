716f9850333f618fabc5f3d97b4f02213f4f7671
iceBear67 <48877375+iceBear67@users.noreply.github.com>
2024-04-27 08:21:27 +0800

    use :cookies:

--- main.py
+++ main.py
@@ -62,6 +62,7 @@ def download_video(message, url, audio=False, format_id="mp4"):
         video_title = round(time.time() * 1000)
         with yt_dlp.YoutubeDL({'format': format_id, 'outtmpl': f'outputs/{video_title}.%(ext)s', 'progress_hooks': [progress], 'postprocessors': [{  # Extract audio using ffmpeg
             'key': 'FFmpegExtractAudio',
+            'cookiefile': '.cookies'
             'preferredcodec': 'mp3',
         }] if audio else [], 'max_filesize': config.max_filesize}) as ydl:
             info = ydl.extract_info(url, download=True)
